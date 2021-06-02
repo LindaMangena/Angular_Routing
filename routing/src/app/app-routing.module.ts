1ac0600160ccd153f355fd5c35176df101516b3b
academy_intern_mac05 <academy_intern_mac05@academy-intern-mac05s-MacBook-Air.local>
2021-06-02 16:06:17 +0200

    added Navigation Between components

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
+
+
+
 const routes: Routes = [
   {path: 'departments', component: DepartmentListComponent},
   {path: 'employees', component:EmployeeListComponent}
@@ -13,5 +16,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export  const routingComponents = [DepartmentListComponent,EmployeeListComponent]
+
+
 
 
